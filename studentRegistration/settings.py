4036ae6b854364c1cfa03c119ce00575fe568350
Prakhar Varshney <prakharnegative@gmail.com>
2018-12-07 01:42:25 +0530

    new

--- studentRegistration/settings.py
+++ studentRegistration/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +126,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS  = [
    STATIC_DIR,
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
